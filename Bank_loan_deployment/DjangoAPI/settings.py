eb92e15ceb3ef22abe23ca96719f16f911dc7686
Bhuwan Acharya <bhuwanacharya56@gmail.com>
2020-12-24 10:38:52 +0100

    Update settings.py

--- Bank_loan_deployment/DjangoAPI/settings.py
+++ Bank_loan_deployment/DjangoAPI/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 's-04fd!r7)e1*vor5eb$8q^celg(vc884o$8654!ua!4e(p9mi'
+SECRET_KEY = #####################
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
